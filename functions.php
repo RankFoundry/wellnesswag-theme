d84b8915001e09eefe063e83c7b34426df79c531
development <kevin.fairbanks@leafygroup.io>
2024-05-03 13:14:06 +0000

    long form CSS fixes

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ defined( 'ABSPATH' ) || exit;
 /*--------------------------------------------------------------*/
 // Define theme version
 if (!defined('WELLNESS_WAG_THEME_VERSION')) {
-    define('WELLNESS_WAG_THEME_VERSION', '1.0.17');
+    define('WELLNESS_WAG_THEME_VERSION', '1.0.18');
 }
 
 // Define theme directory path
