c090e2f6f91e21bcb03dbcaf46b260f34adea488
development <kevin.fairbanks@leafygroup.io>
2024-05-28 13:56:06 +0000

    stripe checkout

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ defined( 'ABSPATH' ) || exit;
 /*--------------------------------------------------------------*/
 // Define theme version
 if (!defined('WELLNESS_WAG_THEME_VERSION')) {
-    define('WELLNESS_WAG_THEME_VERSION', '1.0.21');
+    define('WELLNESS_WAG_THEME_VERSION', '1.0.22');
 }
 
 // Define theme directory path
